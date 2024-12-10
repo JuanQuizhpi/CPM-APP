f2a17e926e183dfb615affc239ebb155ec4ea131
JuanQuizhpi <140675909+JuanQuizhpi@users.noreply.github.com>
2024-12-10 17:21:22 -0500

    Creacion componentes login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import path from 'path';
+import { HomeComponent } from './pages/home/home.component';
+import { SingUpComponent } from './pages/auth/sing-up/sing-up.component';
+import { LogInComponent } from './pages/auth/log-in/log-in.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'auth',
+    children: [
+      { path: 'sing-up', component: SingUpComponent },
+      { path: 'log-in', component: LogInComponent },
+    ],
+  },
+];
---
+++ src/app/pages/auth/sing-up/sing-up.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sing-up',
+  standalone: true,
+  imports: [],
+  templateUrl: './sing-up.component.html',
+  styleUrl: './sing-up.component.css'
+})
+export class SingUpComponent implements OnInit{
+
+
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+}
